a473c3df5266ba399557ad2ff74d7f05d5490294
jamran7 <jamran793@gmail.com>
2017-01-25 16:23:05 -0500

    Marked for Drew
    So I marked what should be changed

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -23,19 +23,22 @@ public class PlayerBehaviour : NetworkBehaviour {
     void Update()
 	{
 		
-        if (!isLocalPlayer)
-        {
+		if (!isLocalPlayer) { // Don't change
 			
 			thisCam.enabled = false;
 			thisAudioListener.enabled = false;
-            return;
-        }
+			return;
+		}
 
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 10.0f;
+		// Drew, this is what you would change {										<-------------- Change
 
-        transform.Rotate(0, 0, -x);
-        transform.Translate(0, z, 0);
+		var x = Input.GetAxis ("Horizontal") * Time.deltaTime * 150.0f;
+		var z = Input.GetAxis ("Vertical") * Time.deltaTime * 10.0f;
+
+		transform.Rotate (0, 0, -x); // these you might keep; not sure, but remember this: if you are going to rotate it must be in the negative z axis or nagate the input (I 	got funny results with posative axis)
+        transform.Translate(0, z, 0); // this is fine, change it or don't
+
+		// }		end of change
 
     }
 	/*
@@ -62,6 +65,9 @@ public class PlayerBehaviour : NetworkBehaviour {
 	}
 
 	*/
+
+
+	// And DONT change the script below
 
 	[ClientRpc]
 	void RpcLerpToPosAndRot(Vector3 pos, Quaternion rot) // This is correct, server not updating though
