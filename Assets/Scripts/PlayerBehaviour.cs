bab68ed07d7c61103d7f4fed16f9b685fde59d22
jamran7 <jamran793@gmail.com>
2017-01-26 15:26:04 -0500

    Needs work
    Need to fix SyncPlayerMovement

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -1,15 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Networking;
 
-public class PlayerBehaviour : NetworkBehaviour {
 
-	[SerializeField] 
-	private Camera thisCam;
+public class PlayerBehaviour : MonoBehaviour {
 
-	[SerializeField]
-	private AudioListener thisAudioListener;
 	/*
 	[SyncVar]
 	private Vector3 playerPos;
@@ -18,18 +13,9 @@ public class PlayerBehaviour : NetworkBehaviour {
 	private Quaternion playerRot;
 	*/
 
-	private float lerpRate = 15f;
-
     void Update()
 	{
 		
-		if (!isLocalPlayer) { // Don't change
-			
-			thisCam.enabled = false;
-			thisAudioListener.enabled = false;
-			return;
-		}
-
 		// Drew, this is what you would change {										<-------------- Change
 
 		var x = Input.GetAxis ("Horizontal") * Time.deltaTime * 150.0f;
@@ -40,56 +26,8 @@ public class PlayerBehaviour : NetworkBehaviour {
 
 		// }		end of change
 
+
     }
-	/*
-	[ClientCallback]
-	void FixedUpdate(){
-		if (isLocalPlayer) {
-			CmdGetPosAndRot (this.transform.position, this.transform.rotation); // give the position
-		}
-		lerpToPosAndRot();
-	}
-
-	[Command]
-	void CmdGetPosAndRot(Vector3 pos, Quaternion rot){
-		playerPos = pos;
-		playerRot = rot;
-
-	}
-
-	void lerpToPosAndRot(){
-		if (!isLocalPlayer) {
-			this.transform.position = Vector3.Lerp (this.transform.position, playerPos, lerpRate * Time.deltaTime);
-			this.transform.rotation = Quaternion.Lerp (this.transform.rotation, playerRot, lerpRate * Time.deltaTime);
-		}
-	}
-
-	*/
-
-
-	// And DONT change the script below
-
-	[ClientRpc]
-	void RpcLerpToPosAndRot(Vector3 pos, Quaternion rot) // This is correct, server not updating though
-	{
-		if (!isLocalPlayer) {
-			this.transform.position = Vector3.Lerp (this.transform.position, pos, lerpRate * Time.deltaTime);
-			this.transform.rotation = Quaternion.Lerp (this.transform.rotation, rot, lerpRate * Time.deltaTime);
-		}
-		CmdGiveNewPositionAndRot (this.transform.position, this.transform.rotation);
-	}
-	[Command]
-	void CmdGiveNewPositionAndRot( Vector3 pos, Quaternion rot){ // I want the client to give back the new position of the other player
-		this.transform.position = Vector3.Lerp (this.transform.position, pos, lerpRate * Time.deltaTime);
-		this.transform.rotation = Quaternion.Lerp (this.transform.rotation, rot, lerpRate * Time.deltaTime);
-	}
-
-	[ServerCallback]
-	void FixedUpdate(){
-		RpcLerpToPosAndRot (this.transform.position, this.transform.rotation); // give client server position of other player
-
-	}
-
 
 
 }
---
+++ Assets/Scripts/SyncPlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SyncPlayerMovement : NetworkBehaviour {
+
+	private float lerpRate = 30f;
+	[SerializeField] 
+	private Camera thisCam;
+
+	[SerializeField]
+	private AudioListener thisAudioListener;
+
+	[ClientRpc]
+	void RpcLerpToPosAndRot(Vector3 pos, Quaternion rot) // This is correct, server not updating though
+	{
+		if (!isLocalPlayer) {
+			this.transform.position = Vector3.Lerp (this.transform.position, pos, lerpRate * Time.deltaTime);
+			this.transform.rotation = Quaternion.Lerp (this.transform.rotation, rot, lerpRate * Time.deltaTime);
+		}
+		//CmdGiveNewPositionAndRot (this.transform.position, this.transform.rotation);
+	}
+
+	[Command]
+	void CmdGiveNewPositionAndRot( Vector3 pos, Quaternion rot){ // I want the client to give back the new position of the other player
+		this.transform.position = Vector3.Lerp (this.transform.position, pos, lerpRate * Time.deltaTime);
+		this.transform.rotation = Quaternion.Lerp (this.transform.rotation, rot, lerpRate * Time.deltaTime);
+	}
+
+	[ServerCallback]
+	void FixedUpdate(){
+		RpcLerpToPosAndRot (this.transform.position, this.transform.rotation); // give client server position of other player
+
+	}
+
+	void Update()
+	{
+		if (!isLocalPlayer) { // Don't change
+
+			thisCam.enabled = false;
+			thisAudioListener.enabled = false;
+			return;
+		}
+	}
+
+}
