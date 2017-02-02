62080a91585d0c5f289e803c33ac427332db8c58
jamran7 <jamran793@gmail.com>
2017-02-01 23:21:35 -0500

    (Don't break) Hudfunctions, lobby player, spectator, and car
    Major changes in the scripts. The main script that acts as a network
    manager is HudFunctions. Added a lobby player, spectator player, and the
    car now spawns. Still needs fixing, but there is now a hud to spawn the
    player as each. There is also a start menu now that allows users to
    create a host or to connect to one.

---
+++ Assets/Scripts/LobbyPlayerBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LobbyPlayerBehaviour : NetworkBehaviour {
+
+
+	[SerializeField] 
+	private Camera thisCam;
+
+	[SerializeField]
+	private AudioListener thisAudioListener;
+
+	void Update()
+	{
+		if (!isLocalPlayer) {
+
+			thisCam.enabled = false;
+			thisAudioListener.enabled = false;
+			return;
+		}
+	}
+}
