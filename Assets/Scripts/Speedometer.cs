eab0b3085baca95c70d0d3c1d50a6b6dff3407d6
jamran7 <jamran793@gmail.com>
2017-02-24 11:22:44 -0500

    Blinkers
    Added blinkers to the script still need to add the physical game object

--- Assets/Scripts/Speedometer.cs
+++ Assets/Scripts/Speedometer.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 public class Speedometer : NetworkBehaviour {
+	
 	public GameObject ICASObject;
 	private float curSpd = 0, desSpd = 0;
 	private bool ICASCtrl = false;
 	public GameObject CurFirstDigit, CurSecondDigit, DesFirstDigit, DesSecondDigit, Car, GreenIndicator, RedIndicator;
 	private Digit d_CurFirstDigit, d_CurSecondDigit, d_DesFirstDigit, d_DesSecondDigit;
-	private bool leftBlinker = false, rightBlinker = false;
+	private bool leftBlinker = false, rightBlinker = false, leftOn = false, rightOn = false;
+	public GameObject lBlinker, rBlinker;
 
 	void Start(){
 		d_CurFirstDigit = CurFirstDigit.GetComponent<Digit> ();
@@ -17,6 +19,8 @@ public class Speedometer : NetworkBehaviour {
 		d_DesSecondDigit = DesSecondDigit.GetComponent<Digit> ();
 		d_CurFirstDigit.displayZero = true;
 		RedIndicator.SetActive (false);
+		renderBlinker (lBlinker, leftOn);
+		renderBlinker (rBlinker, rightOn);
 	}
 	
 	// Update is called once per frame
@@ -37,6 +41,15 @@ public class Speedometer : NetworkBehaviour {
 		}
 	}
 
+	void Update(){
+		if (Input.GetKeyDown ("Q")) {
+			leftBlink ();
+		}
+		if (Input.GetKeyDown ("E")) {
+			rightBlink ();
+		}
+	}
+
 	void DisplayCurSpeed(float spd){
 		int first = (int)spd % 10;
 		Debug.Log (first);
@@ -50,11 +63,36 @@ public class Speedometer : NetworkBehaviour {
 		d_DesFirstDigit.number = first;
 		d_DesSecondDigit.number = second;
 	}
+	void leftBlink(){
+		leftBlinker = leftBlinker ^ true;
+		rightBlinker = false;
+		LeftBlinker ();
+	}
+	void rightBlink(){
+		rightBlinker = rightBlinker ^ true;
+		leftBlinker = false;
+		RightBlinker ();
+	}
+	void renderBlinker(GameObject blinker, bool state){
+		blinker.SetActive (state);
+	}
 	IEnumerator LeftBlinker(){
-
+		while (leftBlinker) {
+			leftOn = leftOn ^ true;
+			renderBlinker (lBlinker, leftOn);
+			yield return null;
+		}
+		leftOn = false;
+		renderBlinker (lBlinker, leftOn);
 	}
 
 	IEnumerator RightBlinker(){
-
+		while (rightBlinker) {
+			rightOn = rightOn ^ true;
+			renderBlinker (rBlinker, rightOn);
+			yield return null;
+		}
+		rightOn = false;
+		renderBlinker (rBlinker, rightOn);
 	}
 }
